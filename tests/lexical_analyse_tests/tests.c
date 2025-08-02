1c18d197c493052ca20416fbd508283916939e86
IlyaManishin <manishin06@gmail.com>
2025-08-02 23:33:36 +0700

    write tests, change test logic

--- tests/lexical_analyse_tests/tests.c
+++ tests/lexical_analyse_tests/tests.c
@@ -4,82 +4,151 @@
 
 #include <tokenizer_api.h>
 
+#define PATH_MAX 128
 #define STRNCMP_MAX 1024
 #define SUCC 1
 #define ERR 0
 
+typedef struct TCheckData
+{
+    TokenTypes check;
+    const char *str;
+} TCheckData;
+
 static int testIndex = 1;
 
-char *get_ident_name(TToken token)
+char *get_token_string(TToken token)
 {
     size_t length = token.end - token.start;
-    char *name = (char *)malloc((length + 1) * sizeof(char));
+    char *tokenStr = (char *)malloc((length + 1) * sizeof(char));
 
     for (int i = 0; i < length; i++)
     {
-        name[i] = *(token.start + i);
+        tokenStr[i] = *(token.start + i);
     }
-    name[length] = '\0';
-    return name;
+    tokenStr[length] = '\0';
+    return tokenStr;
 }
 
-int check_token(TTokenizer *tok, TokenTypes check, const char *checkName)
+int check_token(TTokenizer *tok, int index, TokenTypes check, const char *checkName)
 {
     TToken token = read_token(tok);
-    if (check == IDENT)
+    if (token.type != check)
     {
-        if (token.type != IDENT)
-            return 0;
-        char *name = get_ident_name(token);
-        int res = strcmp(name, checkName);
+        printf("(%d) Incorrect type: %d. Expected: %d\n", index, token.type, check);
+        return ERR;
+    }
+    if (check == IDENT || check == NUMBER)
+    {
+        char *tokenStr = get_token_string(token);
+        int res = strcmp(tokenStr, checkName);
         if (res != 0)
         {
-            printf("Incorrect ident name: %s. Expected: %s\n", name, checkName);
+            if (check == IDENT)
+                printf("(%d) Incorrect ident name: %s. Expected: %s\n", index, tokenStr, checkName);
+            else if (check == NUMBER)
+            {
+                printf("(%d) Incorrect number: %s. Expected: %s\n", index, tokenStr, checkName);
+            }
         }
-        free(name);
+        free(tokenStr);
         return res == 0 ? SUCC : ERR;
-    }
-    else
-    {
-        return token.type == check ? SUCC : ERR;
     }
 }
 
-int test1()
+int base_test(const TCheckData data[], int n)
 {
     int result = SUCC;
-    FILE *file = fopen("test_files/test1.txt", "r");
+
+    char filePath[PATH_MAX];
+    snprintf(filePath, PATH_MAX, "test_files/test%d.txt", testIndex);
+    FILE *file = fopen(filePath, "r");
+
     TTokenizer *tokenizer = tokenizer_from_file(file);
-
-    struct CheckData
-    {
-        TokenTypes check;
-        const char *str
-    } data[] = {
-
-    };
-    int n = sizeof(data) / sizeof(data[0]);
     for (int i = 0; i < n; i++)
     {
-        int r = check_token(tokenizer, data[i].check, data[i].str);
+        int r = check_token(tokenizer, i, data[i].check, data[i].str);
         if (r == ERR)
         {
             result = ERR;
             break;
         }
     }
+    if (check_token(tokenizer, 0, EOF_TOKEN, NULL) == ERR)
+        result = ERR;
     fclose(file);
     return result;
 }
 
+int test1()
+{
+    TCheckData data[] = {
+        {FOR_KW, NULL},
+        {IDENT, "i"},
+        {IN_KW, NULL},
+        {IDENT, "range"},
+        {LPAREN, NULL},
+        {NUMBER, "1"},
+        {COMMA, NULL},
+        {NUMBER, "3"},
+        {RPAREN, NULL},
+        {COLON, NULL},
+        {NEWLINE, NULL},
+        {INDENT, NULL},
+        {IDENT, "print"},
+        {LPAREN, NULL},
+        {NUMBER, "5"},
+        {RPAREN, NULL},
+        {DEDENT, NULL}};
+    int n = sizeof(data) / sizeof(data[0]);
+    return base_test(data, n);
+}
+
 int test2()
 {
-    return SUCC;
+    TCheckData data[] = {
+        {IMPORT_KW, NULL},
+        {IDENT, "foo"},
+        {NEWLINE, NULL},
+        {FUNC_KW, NULL},
+        {IDENT, "f"},
+        {LPAREN, NULL},
+        {RPAREN, NULL},
+        {COLON, NULL},
+        {NEWLINE, NULL},
+        {INDENT, NULL},
+        {IDENT, "print"},
+        {LPAREN, NULL},
+        {NUMBER, "13.4"},
+        {RPAREN, NULL},
+        {NEWLINE, NULL},
+        {DEDENT, NULL}};
+    int n = sizeof(data) / sizeof(data[0]);
+    return base_test(data, n);
 }
 
 int test3()
 {
     return SUCC;
+    TCheckData data[] = {
+    {IDENT, "a"},
+    {PLUS_ASSIGN, NULL},
+    {IDENT, "b"},
+    {NEWLINE, NULL},
+
+    {IDENT, "a"},
+    {EQ, NULL},
+    {NUMBER, "4"},
+    {PLUS, NULL},
+    {NUMBER, "7"},
+    {NEWLINE, NULL},
+
+    {IDENT, "c"},
+    {EQ, NULL},
+    {STRING, "boo"},
+    {NEWLINE, NULL}};
+    int n = sizeof(data) / sizeof(data[0]);
+    return base_test(data, n);
 }
 
 int run_test(int (*test)())
@@ -87,12 +156,13 @@ int run_test(int (*test)())
     int result = test();
     if (result)
     {
-        printf("%d) Success: test%d\n\n", testIndex, testIndex);
+        printf("%d) Test%d : Success\n", testIndex, testIndex);
     }
     else
     {
-        printf("%d) ERROR: test%d\n\n", testIndex, testIndex);
+        printf("%d) Test%d : ERROR\n", testIndex, testIndex);
     }
+    printf("_______________________________\n");
     testIndex++;
     return result;
 }
